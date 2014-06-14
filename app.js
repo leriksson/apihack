c3ed7f58e07f0c11fb8a7480e0402b64e6f250fe
Luc Olson <lusydo@gmail.com>
2014-06-14 19:34:59 -0400

    2ndt commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ $(function() {
 	$('form').submit(function(event) {
 		var searchTerm = $('input[name="tags"]').val();
 		youtubeSearch(searchTerm);
+		$.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + enterSearch + "&key=AIzaSyD32aYdrGJamwS-OgMm5d5nxv1XVfDrN4c");
 	});
 
 });
