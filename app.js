d5791138a77f82d4398b827f401aff9605523ecf
Luc Olson <lusydo@gmail.com>
2014-06-14 23:29:04 -0400

    console log click

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 $(function() {
 
-	var searchresults = xxx;
+	// var searchresults = xxx;
 
-	$('form').submit(function(event) {
-		var searchTerm = $('input[name="tags"]').val();
-		youtubeSearch(searchTerm);
-		$.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + enterSearch + "&key=AIzaSyD32aYdrGJamwS-OgMm5d5nxv1XVfDrN4c");
+	$('input[type="submit"]').submit(function() {
+		// var searchTerm = $('input[name="tags"]').val();
+		// youtubeSearch(searchTerm);
+		console.log('form click');
+		// $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + enterSearch + "&key=AIzaSyD32aYdrGJamwS-OgMm5d5nxv1XVfDrN4c");
 	});
-
+	console.log('form click');
 });
 
 // AJAX Search to youtube with response going to results
 
-function youtubeSearch(enterSearch) {
-	$.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + enterSearch + "&key=AIzaSyD32aYdrGJamwS-OgMm5d5nxv1XVfDrN4c")
-}
+// function youtubeSearch(enterSearch) {
+// 	$.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + enterSearch + "&key=AIzaSyD32aYdrGJamwS-OgMm5d5nxv1XVfDrN4c")
+// }
